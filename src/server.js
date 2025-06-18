2e2be1531ea5d7dab6e6d1564c82e364e498a694
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-06-19 02:47:39 +0300

    STEPS 4,5,6,7 ADDED

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
-// src/server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from "dotenv";
+import { env } from './utils/env.js';
+import { getAllcontacts, getContactById } from './services/contacts.js';
 
-dotenv.config();
-
-const PORT = Number(process.env.PORT);
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
@@ -25,18 +22,60 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello world!',
+      message: 'Welcome to the Contacts APP',
     });
   });
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllcontacts();
+      res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.json({
+        status: 500,
+        message: 'Error fetching contacts',
+        error: error.message,
+      });
+    }
+  });
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const contact = await getContactById(id);
+      if (!contact) {
+        res.json({
+          status: 404,
+          message: 'Contact not found',
+        });
+        return;
+      }
+      res.json({
+        status: 200,
+        message: `Successfully found contact with id: ${id}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.json({
+        status: 500,
+        message: 'Error fetching contact',
+        error: error.message,
+      });
+    }
+  });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
+  app.use((req, res, next) => {
+    res.json({
+      status: 404,
       message: 'Not found',
     });
   });
 
   app.use((err, req, res, next) => {
-    res.status(500).json({
+    res.json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
