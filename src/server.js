5f5cb421dcc60cc3a2892d008189845324afd546
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-06-20 01:45:09 +0300

    status nesne olarak yeniden eklendi

--- src/server.js
+++ src/server.js
@@ -28,15 +28,17 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     try {
       const contacts = await getAllContacts();
-      res.status(200).json({
+        res.json({
+            status: 200,
         message: 'Successfully found contacts!',
         data: contacts,
       });
     } catch (error) {
-      res.status(500).json({
-        message: 'Error fetching contacts',
-        error: error.message,
-      });
+        res.json({
+          status: 500,
+          message: 'Error fetching contacts',
+          error: error.message,
+        });
     }
   });
   app.get('/contacts/:contactId', async (req, res) => {
@@ -44,17 +46,20 @@ export const setupServer = () => {
     try {
       const contact = await getContactById(contactId);
       if (!contact) {
-        res.status(404).json({
+          res.json({
+        status: 404,
           message: 'Contact not found',
         });
         return;
       }
-      res.status(200).json({
+        res.json({
+        status: 200,
         message: `Successfully found contact with id: ${contactId}!`,
         data: contact,
       });
     } catch (error) {
-      res.status(500).json({
+        res.json({
+        status: 500,
         message: 'Error fetching contact',
         error: error.message,
       });
@@ -62,13 +67,15 @@ export const setupServer = () => {
   });
 
   app.use((req, res, next) => {
-    res.status(404).json({
+      res.json({
+    status: 404,
       message: 'Contact not found',
     });
   });
 
   app.use((err, req, res, next) => {
-    res.status(500).json({
+      res.json({
+    status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
