b4dfe484c27b92714b3b825879e8baa9ebd79660
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-07-13 23:14:16 +0300

    server router fixed

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -32,7 +33,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
